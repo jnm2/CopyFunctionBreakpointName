d5256eff4c24f3679ac1c1c309c6176f6bcdf0d9
jnm2 <me@jnm2.com>
2018-07-25 20:51:31 -0400

    Basic test coverage

---
+++ src/CopyFunctionBreakpointName.Tests/AnnotatedSourceUtils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.CodeAnalysis.Text;
+
+namespace CopyFunctionBreakpointName.Tests
+{
+    internal static class AnnotatedSourceUtils
+    {
+        public const string AnnotationStartMarker = "[|";
+        public const string AnnotationEndMarker = "|]";
+
+        public static (string source, TextSpan span) Parse(string annotatedSource, string paramName)
+        {
+            if (!TryParse(annotatedSource, out var source, out var span))
+            {
+                throw new ArgumentException(
+                    "The source must be annotated with \"" + AnnotationStartMarker + "\" and \"" + AnnotationEndMarker + "\" around the selected text.",
+                    paramName);
+            }
+
+            return (source, span);
+        }
+
+        public static bool TryParse(string annotatedSource, out string unannotatedSource, out TextSpan span)
+        {
+            if (TrySingleIndexOf(annotatedSource, AnnotationStartMarker, out var annotationStart)
+                && TrySingleIndexOf(annotatedSource, AnnotationEndMarker, out var annotationEnd))
+            {
+                var innerSubstringStart = annotationStart + AnnotationStartMarker.Length;
+                var innerSubstringLength = annotationEnd - innerSubstringStart;
+
+                if (innerSubstringLength >= 0)
+                {
+                    unannotatedSource =
+                        annotatedSource.Substring(0, annotationStart)
+                        + annotatedSource.Substring(innerSubstringStart, innerSubstringLength)
+                        + annotatedSource.Substring(annotationEnd + AnnotationEndMarker.Length);
+
+                    span = new TextSpan(annotationStart, innerSubstringLength);
+
+                    return true;
+                }
+            }
+
+            unannotatedSource = null;
+            span = default;
+            return false;
+        }
+
+        private static bool TrySingleIndexOf(string str, string value, out int index)
+        {
+            if (str == null) throw new ArgumentNullException(nameof(str));
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            index = str.IndexOf(value, StringComparison.Ordinal);
+            if (index == -1) return false;
+
+            if (str.IndexOf(value, index + value.Length, StringComparison.Ordinal) == -1) return true;
+
+            index = -1;
+            return false;
+        }
+    }
+}
---
+++ src/CopyFunctionBreakpointName.Tests/FunctionBreakpointUtilsTests.cs
@@ -0,0 +1,356 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+
+namespace CopyFunctionBreakpointName.Tests
+{
+    public static class FunctionBreakpointUtilsTests
+    {
+        [Test]
+        public static void Namespace_not_needed()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    void [|B|]() { }
+}", "A.B");
+        }
+
+        [Test]
+        public static void Simple_namespace()
+        {
+            AssertFunctionBreakpointName(@"
+namespace A
+{
+    class B
+    {
+        void [|C|]() { }
+    }
+}", "A.B.C");
+        }
+
+        [Test]
+        public static void Dotted_namespace()
+        {
+            AssertFunctionBreakpointName(@"
+namespace A.B
+{
+    class C
+    {
+        void [|D|]() { }
+    }
+}", "A.B.C.D");
+        }
+
+        [Test]
+        public static void Nested_namespace()
+        {
+            AssertFunctionBreakpointName(@"
+namespace A
+{
+    namespace B1 { }
+
+    namespace B2
+    {
+        class C
+        {
+            void [|D|]() { }
+        }
+    }
+}", "A.B2.C.D");
+        }
+
+        [Test]
+        public static void Nested_dotted_namespace()
+        {
+            AssertFunctionBreakpointName(@"
+namespace A.B
+{
+    namespace C.D
+    {
+        class E
+        {
+            void [|F|]() { }
+        }
+    }
+}", "A.B.C.D.E.F");
+        }
+
+        [Test]
+        public static void Nested_classes()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    class B
+    {
+        void [|C|]() { }
+    }
+}", "A.B.C");
+        }
+
+        [Test]
+        public static void Struct()
+        {
+            AssertFunctionBreakpointName(@"
+struct A
+{
+    void [|B|]() { }
+}", "A.B");
+        }
+
+        [Test]
+        public static void Nested_structs()
+        {
+            AssertFunctionBreakpointName(@"
+struct A
+{
+    struct B
+    {
+        void [|C|]() { }
+    }
+}", "A.B.C");
+        }
+
+        [Test]
+        public static void Namespace_identifier_selection_returns_nothing()
+        {
+            AssertFunctionBreakpointName(@"
+namespace [|A|]
+{
+    class B
+    {
+        void C() { }
+    }
+}", null);
+        }
+
+        [Test]
+        public static void Class_identifier_selection_returns_nothing()
+        {
+            AssertFunctionBreakpointName(@"
+class [|A|]
+{
+    void B() { }
+}", null);
+        }
+
+        [Test]
+        public static void Zero_width_selection_at_start_of_method_name()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    void [||]B() { }
+}", "A.B");
+        }
+
+        [Test]
+        public static void Zero_width_selection_at_end_of_method_name()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    void B[||]() { }
+}", "A.B");
+        }
+
+        [Test]
+        public static void Zero_width_selection_inside_method_name()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    void B[||]B() { }
+}", "A.BB");
+        }
+
+        [Test]
+        public static void Selection_past_end_of_method_name_returns_nothing()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    void [|B(|]) { }
+}", null);
+        }
+
+        [Test]
+        public static void Selection_before_start_of_method_name_returns_nothing()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    void[| B|]() { }
+}", null);
+        }
+
+        [Test]
+        public static void Local_function_returns_nothing()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    void B()
+    {
+        void [|C|]() { }
+    }
+}", null);
+        }
+
+        [Test]
+        public static void Entire_property()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    int [|B|] { get; set; }
+}", "A.B");
+        }
+
+        [Test]
+        public static void Entire_property_expression()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    int [|B|] => 0;
+}", "A.B");
+        }
+
+        [Test]
+        public static void Get_accessor_auto()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    int B { [|get|]; }
+}", "A.B.get");
+        }
+
+        [Test]
+        public static void Get_accessor_expression()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    int B { [|get|] => 0; }
+}", "A.B.get");
+        }
+
+        [Test]
+        public static void Get_accessor_statement()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    int B { [|get|] { return 0; } }
+}", "A.B.get");
+        }
+
+        [Test]
+        public static void Set_accessor_auto()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    int B { [|set|]; }
+}", "A.B.set");
+        }
+
+        [Test]
+        public static void Set_accessor_expression()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    int B { [|set|] => _ = 0; }
+}", "A.B.set");
+        }
+
+        [Test]
+        public static void Set_accessor_statement()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    int B { [|set|] { } }
+}", "A.B.set");
+        }
+
+        [Test]
+        public static void Entire_event_returns_nothing()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    event System.Action [|B|];
+}", null);
+        }
+
+        [Test]
+        public static void Entire_event_custom_returns_nothing()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    event System.Action [|B|] { add { } remove { } }
+}", null);
+        }
+
+        [Test]
+        public static void Add_accessor_expression()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    event System.Action B { [|add|] => _ = value; remove => _ = value; }
+}", "A.add_B");
+        }
+
+        [Test]
+        public static void Add_accessor_statement()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    event System.Action B { [|add|] { } remove { } }
+}", "A.add_B");
+        }
+
+        [Test]
+        public static void Remove_accessor_expression()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    event System.Action B { add => _ = value; [|remove|] => _ = value; }
+}", "A.remove_B");
+        }
+
+        [Test]
+        public static void Remove_accessor_statement()
+        {
+            AssertFunctionBreakpointName(@"
+class A
+{
+    event System.Action B { add { } [|remove|] { } }
+}", "A.remove_B");
+        }
+
+        private static void AssertFunctionBreakpointName(string annotatedSource, string expected)
+        {
+            Assert.That(GetFunctionBreakpointName(annotatedSource), Is.EqualTo(expected));
+        }
+
+        private static string GetFunctionBreakpointName(string annotatedSource)
+        {
+            var (source, span) = AnnotatedSourceUtils.Parse(annotatedSource, nameof(annotatedSource));
+
+            var factory = FunctionBreakpointUtils.GetFunctionBreakpointNameFactory(
+                CSharpSyntaxTree.ParseText(source).GetRoot(),
+                span);
+
+            return factory?.ToString();
+        }
+    }
+}
