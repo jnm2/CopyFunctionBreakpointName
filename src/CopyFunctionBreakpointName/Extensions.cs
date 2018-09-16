8030e0a39b9da34658ed03337cfd426292d47955
jnm2 <me@jnm2.com>
2018-09-16 17:03:57 -0400

    Remove unused usings

--- src/CopyFunctionBreakpointName/Extensions.cs
+++ src/CopyFunctionBreakpointName/Extensions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.Threading;
+﻿using System.Threading.Tasks;
 
 namespace CopyFunctionBreakpointName
 {
