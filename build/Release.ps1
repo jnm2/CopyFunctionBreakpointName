1a1092f897353d6b0df8b6a744773cf2559e22fe
jnm2 <me@jnm2.com>
2018-11-19 22:51:06 -0500

    Release to GitHub and tag

--- build/Release.ps1
+++ build/Release.ps1
@@ -8,3 +8,51 @@ function PublishToMarketplace(
     $vsixPublisher = Join-Path $visualStudioInstallation 'VSSDK\VisualStudioIntegration\Tools\Bin\VsixPublisher.exe'
     & $vsixPublisher publish -payload $VsixPath -publishManifest $VsixPublishManifestPath -personalAccessToken $VsixMarketplaceAccessToken
 }
+
+function CreateGitHubRelease(
+    [Parameter(Mandatory=$true)] [string] $GitHubAccessToken,
+    [Parameter(Mandatory=$true)] [string] $Owner,
+    [Parameter(Mandatory=$true)] [string] $Repository,
+    [Parameter(Mandatory=$true)] [string] $Commit,
+    [Parameter(Mandatory=$true)] [string] $Tag,
+    [Parameter(Mandatory=$true)] [string] $Name,
+    [string] $Body,
+    [switch] $Draft,
+    [switch] $Prerelease,
+    [string[]] $Assets
+) {
+    [System.Net.ServicePointManager]::SecurityProtocol = 'tls12'
+
+    $headers = @{
+        Accept = 'application/vnd.github.v3+json'
+        Authorization = "token $GitHubAccessToken"
+        'User-Agent' = 'PowerShell'
+    }
+
+    # https://developer.github.com/v3/repos/releases/#create-a-release
+    $creationResult = Invoke-RestMethod `
+        -Method 'post' `
+        -Uri "https://api.github.com/repos/$Owner/$Repository/releases" `
+        -Headers $headers `
+        -Body (ConvertTo-Json @{
+            tag_name = $Tag
+            target_commitish = $Commit
+            name = $Name
+            body = $Body
+            draft = $Draft.IsPresent
+            prerelease = $Prerelease.IsPresent
+        })
+
+    foreach ($asset in $Assets) {
+        # https://developer.github.com/v3/repos/releases/#upload-a-release-asset
+        $filename = Split-Path $asset -leaf
+        $uploadUrl = $creationResult.upload_url -replace "\{\?[^}]+\}", "?name=$filename"
+
+        $null = Invoke-RestMethod `
+            -Method 'post' `
+            -Uri $uploadUrl `
+            -Headers $headers `
+            -ContentType 'application/zip' `
+            -InFile $asset
+    }
+}
--- build/ReleasePipeline.ps1
+++ build/ReleasePipeline.ps1
@@ -3,12 +3,30 @@
 
 # Secret release variables are injected
 Param(
-    [Parameter(Mandatory=$true)] [string] $VsixMarketplaceAccessToken
+    [Parameter(Mandatory=$true)] [string] $VsixMarketplaceAccessToken,
+    [Parameter(Mandatory=$true)] [string] $GitHubReleaseAccessToken
 )
 
 . "$PSScriptRoot\Release.ps1"
+
+# Calculate release version by scripping the build metadata from the build version
+# (of the form ‘1.0.0+build.1234.commit.abcdef12’)
+$releaseVersion = $env:BUILD_BUILDNUMBER.Substring(0, $env:BUILD_BUILDNUMBER.IndexOfAny(('-', '+')))
 
 PublishToMarketplace `
     "$env:SYSTEM_ARTIFACTSDIRECTORY\CI\VSIX\CopyFunctionBreakpointName.vsix" `
     "$env:SYSTEM_ARTIFACTSDIRECTORY\CI\Marketplace\VsixPublishManifest.json" `
     $VsixMarketplaceAccessToken
+
+CreateGitHubRelease `
+    -GitHubAccessToken $GitHubReleaseAccessToken `
+    -Owner 'jnm2' `
+    -Repository 'CopyFunctionBreakpointName' `
+    -Commit $env:BUILD_SOURCEVERSION `
+    -Tag "v$releaseVersion" `
+    -Name $releaseVersion `
+    -Body 'https://marketplace.visualstudio.com/items?itemName=jnm2.CopyFunctionBreakpointName' `
+    -Assets (
+        "$env:SYSTEM_ARTIFACTSDIRECTORY\CI\VSIX\CopyFunctionBreakpointName.vsix",
+        "$env:SYSTEM_ARTIFACTSDIRECTORY\CI\Symbols\CopyFunctionBreakpointName.pdb"
+    )
