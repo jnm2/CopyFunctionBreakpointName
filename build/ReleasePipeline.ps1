1d9dc20c3ffb002642ea6ebc974d7ec892035948
jnm2 <me@jnm2.com>
2018-11-19 23:11:16 -0500

    Document release pipeline configuration in source control

--- build/ReleasePipeline.ps1
+++ build/ReleasePipeline.ps1
@@ -1,5 +1,31 @@
-# Until release pipelines have YAML support (https://dev.azure.com/mseng/Azure%20DevOps%20Roadmap/_workitems/edit/1221170),
-# as much as possible goes here.
+<#
+Until release pipelines have YAML support (https://dev.azure.com/mseng/Azure%20DevOps%20Roadmap/_workitems/edit/1221170),
+as much as possible goes here.
+
+Setup instructions:
+
+1. Create secret release variables:
+
+   - VsixMarketplaceAccessToken, value obtained from Azure DevOps user settings (Security/Personal Access Tokens).
+     Organization *must* be changed to ‘All accessible organizations’ at creation time. Editing later doesn’t work.
+     Under Scopes, choose ‘Custom defined,’ ‘Show all scopes,’ and click Publish under the Marketplace group.
+
+   - GitHubReleaseAccessToken, value obtained from https://github.com/settings/tokens.
+     Only scope needed is public_repo.
+
+2. Add PowerShell task to the release pipeline job and set:
+
+   - Display name: Delegate to source-controlled file
+
+   - Script path: $(System.DefaultWorkingDirectory)\CI\ReleasePipelineSource\ReleasePipeline.ps1
+     This assumes the build producing the artifacts is named CI and that one of the artifacts contains this file
+     and is named ReleasePipelineSource. Refer to azure_pipelines.yml to see how the artifacts are created.
+
+   - Arguments: "$(VsixMarketplaceAccessToken)" "$(GitHubReleaseAccessToken)"
+
+3. If you like, set Options > General > Release name format: Release $(Build.BuildNumber)
+   Should be good to go!
+#>
 
 # Secret release variables are injected
 Param(
